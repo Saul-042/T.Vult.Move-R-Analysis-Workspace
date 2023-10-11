a6ecb3c0d6a28fad525977e6fffd8447e9a4595b
Saul-042 <saulglez02@gmail.com>
2023-10-11 16:28:51 -0500

    Start 1

---
+++ Scripts/01. Import and Clean Data.R
@@ -0,0 +1,6 @@
+##################################################
+# T.VULTURE MOVE ANALYSIS. Script 01: DATA CLEAN #
+##################################################
+## Cleans raw movement data and converts the output .csv file from Movebank-organized data to Move format
+## It will eliminate duplicates and incomplete records, as well as multi-locations
+
