9064cc45b29a8bcda5da2c713c6f946c09376aa6
Saul-042 <saulglez02@gmail.com>
2023-10-11 16:36:00 -0500

    Start 2

--- Scripts/00. Download Data.R
+++ Scripts/00. Download Data.R
@@ -1,6 +1,8 @@
-#################################################
-##    DOWNLOAD MOVEMENT DATA FROM MOVEBANK     ##
-#################################################
+#####################################################
+# T.VULTURE MOVE ANALYSIS. Script 00: DATA DOWNLOAD #
+#####################################################
+## Downloads the movement data from the Movebank online repository.
+# beta (testing use)
 
 # Packages ####
 library(move)
--- Scripts/01. Import and Clean Data.R
+++ Scripts/01. Import and Clean Data.R
@@ -4,3 +4,6 @@
 ## Cleans raw movement data and converts the output .csv file from Movebank-organized data to Move format
 ## It will eliminate duplicates and incomplete records, as well as multi-locations
 
+# Packages ####
+library(move)
+
