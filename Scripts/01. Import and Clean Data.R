689efc0665e08a030e1fe0932f703944ad972fe4
Saul-042 <103217427+Saul-042@users.noreply.github.com>
2023-10-12 12:09:46 -0500

    Update 01. Import and Clean Data.R

--- Scripts/01. Import and Clean Data.R
+++ Scripts/01. Import and Clean Data.R
@@ -4,5 +4,6 @@
 ## Cleans raw movement data and converts the output .csv file from Movebank-organized data to Move format
 ## It will eliminate duplicates and incomplete records, as well as multi-locations
 
-# Packages ####
+# Packages -----------------------------------------------------------------------------------------
 library(move)
+library(tidyverse)
