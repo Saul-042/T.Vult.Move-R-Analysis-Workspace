7f09c86cfaa9f85c5da9acae869502919d1e3c63
Saul-042 <saulglez02@gmail.com>
2023-10-15 16:30:51 -0500

    Update Scripts

--- Scripts/00. Download Data.R
+++ Scripts/00. Download Data.R
@@ -1,11 +1,48 @@
 #####################################################
-# T.VULTURE MOVE ANALYSIS. Script 00: DATA DOWNLOAD #
+# T.VULTURE MOVE ANALYSIS. Script 00: DOWNLOAD DATA #
 #####################################################
-## Downloads the movement data from the Movebank online repository.
-# beta (testing use)
+## Downloads the movement data directly from the Movebank online repository.
 
-# Packages ####
+#beta (Script under testing, not intended for use)
+# Note(S42): Doesn't work with UH Proxy, Connection timeout error message. There is a chunk of code dedicated to deal with proxys (the possible cause of the problem) in the Script of the Lesson 02, Animove 22.
+
+
+# Packages --------------------------------------------------------------------------------------------
 library(move)
 
-### Sign in into Movebank ####
-cred <- 
+
+# 1. Sign in into Movebank ----------------------------------------------------------------------------
+credentials <- movebankLogin() # Store the username and password for login to Movebank. 
+                               
+## Browse the database for studies using keywords
+searchMovebankStudies(x = "Cuba", login = credentials) 
+
+
+# 2. Get information from a specific dataset ----------------------------------------------------------
+
+### Check the metadata of the study/dataset
+View(getMovebankStudy(study = "Cathartes aura MPIAB Cuba",login = credentials))
+
+### Check for all reference data of animals, deployments and tags
+View(getMovebankReferenceTable(study = "Cathartes aura MPIAB Cuba",login = credentials)[1:4,])
+
+#### Check reduced reference data of animals 
+#### (A subset of the last table, with more specific data for the tracked animals)
+View(getMovebankAnimals(study = "Cathartes aura MPIAB Cuba",login = credentials)[1:4,])
+
+
+# 3. Download the movement data -----------------------------------------------------------------------
+
+### Download the whole dataset
+vult <- getMovebankData(study = "Cathartes aura MPIAB Cuba",login = credentials)
+
+### Download a dataset from a specific animal
+vult9178 <- getMovebankData(study = "Cathartes aura MPIAB Cuba",login = credentials, 
+                            animalName = "9178")
+
+### Download a dataset from a specific animal in a defined time period
+### e.g. between "2022-03-01 00:00:00" and "2022-03-15 00:00:00". Time format: 'yyyyMMddHHmmssSSS'
+vult9178_1 <- getMovebankData(study = "Cathartes aura MPIAB Cuba", login = credentials, 
+                              local_identifier = "Cuba-7198", 
+                              timestamp_start = "20220301000000000",
+                              timestamp_end = "20220315000000000")
--- Scripts/01. Import and Clean Data.R
+++ Scripts/01. Import and Clean Data.R
@@ -1,9 +1,59 @@
 ##################################################
-# T.VULTURE MOVE ANALYSIS. Script 01: DATA CLEAN #
+# T.VULTURE MOVE ANALYSIS. Script 01: CLEAN DATA #
 ##################################################
 ## Cleans raw movement data and converts the output .csv file from Movebank-organized data to Move format
 ## It will eliminate duplicates and incomplete records, as well as multi-locations
 
+
 # Packages -----------------------------------------------------------------------------------------
 library(move)
 library(tidyverse)
+
+
+# 1. Import the data -------------------------------------------------------------------------------
+# The data has already been downloaded from the Movebank repository in '.csv' format
+
+## Directly search and choose the '.csv' file in the directory
+TVult.df1 <- read.csv(choose.files())
+
+### Put together split datasets -----------------------------------------
+#### This section is built for datasets which are too large to download in one single file
+##### PROTOCOL: Download the data from Movebank in chunks representing single natural years (e.g. 2021, 2022, ...)
+##### This code imports each year individually and stacks them up in a single dataframe
+## Choose the second year of data (likely 2022)
+TVult.df2 <- read.csv(choose.files())
+## Choose the third year of data (likely 2023)
+TVult.df3 <- read.csv(choose.files())
+## Merge all the imported datasets, use one option depending of number of parts the dataset was divided by
+TVult.df1 <- rbind(TVult.df1, TVult.df2) ## OR ##
+TVult.df1 <- rbind(TVult.df1, TVult.df2, TVult.df3)
+## Delete non-necessary additional datasets from the Environment to save memory
+rm(TVult.df2)
+rm(TVult.df2, TVult.df3)
+
+
+# 2. Preparing the data ----------------------------------------------------------------------------
+
+## Checking Timestamps
+
+### Check the starting date
+min(TVult.df1$timestamp)
+### Check the final date
+max(TVult.df1$timestamp)
+
+## Convert the date to POSIXct format
+TVult.df1$timestamp <- as.POSIXct(TVult.df1$timestamp, format="%F %T", tz="UTC")
+
+# 3. PROCESSING DATA ---------------------------------------------------------------------------
+## Delete all observations with missing lat, long or timestamp values
+indx.cmplt <- complete.cases(TVult.df1[c("location.lat", "location.long", "timestamp"),])
+TVult.df1.complt <- TVult.df1[indx.cmplt == TRUE,]
+
+## Check if there is any duplicated timestamps
+any(duplicated(TVult.df1.complt[c("timestamp","individual.local.identifier")]))
+
+## 03 -> Eliminate duplicated observations (both exact or incomplete) by comparing location and timestamps ----
+TVult.df1.complt <- TVult.df1.complt[!duplicated(TVult.df1.complt[c("timestamp", 
+                                                                    "location.long", 
+                                                                    "location.lat", 
+                                                                    "individual.local.identifier")]),]
