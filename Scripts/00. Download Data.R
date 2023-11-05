d1c8be1fa9b37c654e7b81fba3fdd17811712285
Saul-042 <saulglez02@gmail.com>
2023-11-05 16:59:17 -0500

    Updating Scripts

--- Scripts/00. Download Data.R
+++ Scripts/00. Download Data.R
@@ -1,48 +1,95 @@
-#####################################################
+#Title###############################################
 # T.VULTURE MOVE ANALYSIS. Script 00: DOWNLOAD DATA #
-#####################################################
+#Function############################################
 ## Downloads the movement data directly from the Movebank online repository.
 
-#beta (Script under testing, not intended for use)
-# Note(S42): Doesn't work with UH Proxy, Connection timeout error message. There is a chunk of code dedicated to deal with proxys (the possible cause of the problem) in the Script of the Lesson 02, Animove 22.
+#(S42 NOTE): Doesn't work with UH Proxy, Connection timeout error message. There is a chunk of code dedicated to deal with proxys (the possible cause of the problem) in the Animove Script (ref: Lesson_2_2022).
+
+# `Move2` (New) Workflow --------------------------------------------------------------------------------
+## Packages --------------------------------------------------------------------------------------------
+library(move2)
+library(dplyr)
+
+# 1. Sign in into Movebank ----------------------------------------------------------------------------
+movebank_store_credentials("Saul42") # Store and save in the key-ring the credentials for the login to Movebank.
 
 
-# Packages --------------------------------------------------------------------------------------------
+## 2. Get information from a specific dataset ----------------------------------------------------------
+#### Check the metadata of the study/dataset
+movebank_download_study_info(study_id = 1393954358) %>% 
+  print(width = Inf)
+
+#### Check for reference data of animals, deployments and tags
+movebank_download_deployment(study_id = 1393954358)
+
+
+## 3. Download the movement data -----------------------------------------------------------------------
+### NOTE: Downloading the whole dataset is not recommended, as it's a large amount of data that will take a
+###  lot of time and might crash R.
+
+#### Download a dataset from a specific animal
+movebank_download_study(study_id = 1918503,
+                        attributes = NULL,
+                        sensor_type_id = "radio-transmitter",
+                        #individual_id = c(1918727, 1918739))
+                        individual_local_identifier = c(239,360))
+
+#### Download a dataset from a specific animal in a defined time period
+#### e.g. between "2022-03-01 00:00:00" and "2022-03-15 00:00:00".
+
+
+
+# `Move` (Old) Workflow --------------------------------------------------------------------------------
+## Packages --------------------------------------------------------------------------------------------
 library(move)
 
 
-# 1. Sign in into Movebank ----------------------------------------------------------------------------
+## 1. Sign in into Movebank ----------------------------------------------------------------------------
 credentials <- movebankLogin() # Store the username and password for login to Movebank. 
                                
 ## Browse the database for studies using keywords
 searchMovebankStudies(x = "Cuba", login = credentials) 
 
 
-# 2. Get information from a specific dataset ----------------------------------------------------------
+## 2. Get information from a specific dataset ----------------------------------------------------------
 
-### Check the metadata of the study/dataset
+#### Check the metadata of the study/dataset
 View(getMovebankStudy(study = "Cathartes aura MPIAB Cuba",login = credentials))
 
-### Check for all reference data of animals, deployments and tags
+#### Check for all reference data of animals, deployments and tags
 View(getMovebankReferenceTable(study = "Cathartes aura MPIAB Cuba",login = credentials)[1:4,])
 
-#### Check reduced reference data of animals 
-#### (A subset of the last table, with more specific data for the tracked animals)
+##### Check reduced reference data of animals 
+##### (A subset of the last table, with more specific data for the tracked animals)
 View(getMovebankAnimals(study = "Cathartes aura MPIAB Cuba",login = credentials)[1:4,])
 
 
-# 3. Download the movement data -----------------------------------------------------------------------
+## 3. Download the movement data -----------------------------------------------------------------------
+### NOTE: Downloading the whole dataset is not recommended, as it's a large amount of data that will take a
+###  lot of time and might crash R. 
 
-### Download the whole dataset
+#### Download the whole dataset (NOT RECOMMENDED UNLESS REALLY GOOD INTERNET IS AVAILABLE)
 vult <- getMovebankData(study = "Cathartes aura MPIAB Cuba",login = credentials)
 
-### Download a dataset from a specific animal
+#### Download a dataset from a specific animal
 vult9178 <- getMovebankData(study = "Cathartes aura MPIAB Cuba",login = credentials, 
                             animalName = "9178")
 
-### Download a dataset from a specific animal in a defined time period
-### e.g. between "2022-03-01 00:00:00" and "2022-03-15 00:00:00". Time format: 'yyyyMMddHHmmssSSS'
-vult9178_1 <- getMovebankData(study = "Cathartes aura MPIAB Cuba", login = credentials, 
-                              local_identifier = "Cuba-7198", 
-                              timestamp_start = "20220301000000000",
-                              timestamp_end = "20220315000000000")
+#### Download a dataset from a specific animal in a defined time period
+#### e.g. between "2022-03-01 00:00:00" and "2022-03-15 00:00:00". Time format: 'yyyyMMddHHmmssSSS'
+vult9178.cut <- getMovebankData(study = "Cathartes aura MPIAB Cuba", login = credentials, 
+                               local_identifier = "Cuba-7198", 
+                               timestamp_start = "20220301000000000",
+                               timestamp_end = "20220315000000000")
+
+## 4. Download the NON-LOCATION data -------------------------------------------------------------------
+
+### Check sensors available in a specific study
+getMovebankSensors(study = "Cathartes aura MPIAB Cuba", login = cred) [1:10,]
+
+### As the sensors are listed using a code, access the list of all available sensor types on Movebank and their codes
+getMovebankSensors(login = cred) [,3:5]
+
+### Download accelerometer data as a data.frame
+vult9178.acc <- getMovebankNonLocationData(study = "Cathartes aura MPIAB Cuba", sensorID = "Acceleration", # (S42 NOTE) #QSTN #It's Acceleration the name of the SensorID in the T.Vulture case?
+                                           animalName = "9178", login = cred)
--- Scripts/01. Import and Clean Data.R
+++ Scripts/01. Import and Clean Data.R
@@ -1,59 +1,142 @@
-##################################################
+#Title############################################
 # T.VULTURE MOVE ANALYSIS. Script 01: CLEAN DATA #
-##################################################
-## Cleans raw movement data and converts the output .csv file from Movebank-organized data to Move format
-## It will eliminate duplicates and incomplete records, as well as multi-locations
+#Function#########################################
+## Converts the downloaded .csv file from Movebank to a Move object
+## Cleans raw movement data and eliminate duplicates and incomplete records, as well as multi-locations
 
-
-# Packages -----------------------------------------------------------------------------------------
+# `Move` (Old) Workflow -----------------------------------------------------------------------------
+## Packages -----------------------------------------------------------------------------------------
 library(move)
 library(tidyverse)
 
 
-# 1. Import the data -------------------------------------------------------------------------------
-# The data has already been downloaded from the Movebank repository in '.csv' format
+## 1. Import the data -------------------------------------------------------------------------------
+## The data has been downloaded from the Movebank repository in '.csv' format
 
-## Directly search and choose the '.csv' file in the directory
-TVult.df1 <- read.csv(choose.files())
+### Directly search and choose the '.csv' files in the directory
+TVult <- read.csv(choose.files())
+### Check the structure of the dataset
+str(TVult)
 
-### Put together split datasets -----------------------------------------
-#### This section is built for datasets which are too large to download in one single file
-##### PROTOCOL: Download the data from Movebank in chunks representing single natural years (e.g. 2021, 2022, ...)
-##### This code imports each year individually and stacks them up in a single dataframe
-## Choose the second year of data (likely 2022)
-TVult.df2 <- read.csv(choose.files())
-## Choose the third year of data (likely 2023)
-TVult.df3 <- read.csv(choose.files())
-## Merge all the imported datasets, use one option depending of number of parts the dataset was divided by
-TVult.df1 <- rbind(TVult.df1, TVult.df2) ## OR ##
-TVult.df1 <- rbind(TVult.df1, TVult.df2, TVult.df3)
-## Delete non-necessary additional datasets from the Environment to save memory
-rm(TVult.df2)
-rm(TVult.df2, TVult.df3)
+#### Put together split datasets -----------------------------------------
+##### This section is built for datasets which are too large to download in one single file
+###### PROTOCOL: Download the data from Movebank in chunks representing single natural years (e.g. 2021, 2022, ...)
+###### This code imports each year individually and stacks them up in a single dataframe
+### Choose the second year of data (likely 2022)
+TVult.2 <- read.csv(choose.files())
+### Choose the third year of data (likely 2023)
+TVult.3 <- read.csv(choose.files())
+### Merge all the imported datasets, use one option depending of number of parts the dataset was divided by
+TVult <- rbind(TVult, TVult.2) ## OR ##
+TVult <- rbind(TVult, TVult.2, TVult.3)
+### Delete non-necessary additional datasets from the Environment to save memory
+rm(TVult.2) ## OR ##
+rm(TVult.2, TVult.3)
 
 
-# 2. Preparing the data ----------------------------------------------------------------------------
+## 2. Pre-process the data ----------------------------------------------------------------------------
 
-## Checking Timestamps
+### Check and Correct Time stamps
 
-### Check the starting date
-min(TVult.df1$timestamp)
-### Check the final date
-max(TVult.df1$timestamp)
+#### Check the starting date
+min(TVult$timestamp)
+#### Check the final date
+max(TVult$timestamp)
 
-## Convert the date to POSIXct format
-TVult.df1$timestamp <- as.POSIXct(TVult.df1$timestamp, format="%F %T", tz="UTC")
+#### Convert the date to POSIXct format
+TVult$timestamp <- as.POSIXct(TVult$timestamp, format = "%F %T", tz = "UTC")
+#### Order the time stamps in chronological order
+TVult <- TVult[order(TVult$timestamp),]
 
-# 3. PROCESSING DATA ---------------------------------------------------------------------------
-## Delete all observations with missing lat, long or timestamp values
-indx.cmplt <- complete.cases(TVult.df1[c("location.lat", "location.long", "timestamp"),])
-TVult.df1.complt <- TVult.df1[indx.cmplt == TRUE,]
+### Eliminate entries with missing values and duplicates
 
-## Check if there is any duplicated timestamps
-any(duplicated(TVult.df1.complt[c("timestamp","individual.local.identifier")]))
+#### Store the index of all observations with missing values of latitude, longitude or time stamp
+index.complt <- complete.cases(TVult[,c("location.lat", "location.long", "timestamp")])
+#### Delete all observations with missing values
+TVult <- TVult[index.complt == TRUE,]
 
-## 03 -> Eliminate duplicated observations (both exact or incomplete) by comparing location and timestamps ----
-TVult.df1.complt <- TVult.df1.complt[!duplicated(TVult.df1.complt[c("timestamp", 
-                                                                    "location.long", 
-                                                                    "location.lat", 
-                                                                    "individual.local.identifier")]),]
+#### Check if there is any duplicated time stamps
+any(duplicated(TVult[c("timestamp")]))
+
+##### Build an overview of the duplicates and identify the first one ###
+dup <- getDuplicatedTimestamps(TVult)
+table(unlist(lapply(dup,function(x)length(x))))
+dup[1]
+TVult[dup[[1]],]
+
+#### Eliminate all exact duplicated entries (that share all data except their event-specific ID)
+TVult <- TVult[!duplicated(TVult[,!names(TVult) %in% "event.id"]),]
+
+#### Identify and store first non-exact duplicate (i.e. multi-locations)
+TVult.dup <- anyDuplicated(TVult[,"timestamp"])
+
+#### Check the locations of the identified multi-location's time stamp plus a couple of entries before and after 
+TVult[(TVult.dup-2):(TVult.dup+2),c("timestamp", "location.long", "location.lat", 
+                                    "study.local.timestamp")]
+
+#### Remove all the multi-locations by choosing the entry that minimize the distance between neighboring locations
+while(length(dup <- getDuplicatedTimestamps(TVult))>0){
+  allrowsTOremove <- lapply(1:length(dup), function(x){
+    ## row numbers of duplicates
+    rown <- dup[[x]]
+    ## create a row number ID to find the duplicated time stamps in the subset per individual
+    TVult$rowNumber <- 1:nrow(TVult)
+    ## if the duplicated positions are in the middle of the table
+    if(TVult$rowNumber[1]<rown[1] & TVult$rowNumber[nrow(TVult)]>max(rown)){
+      ### calculate total distance between fix before/after and the first alternate location
+      dist1 <- sum(distHaversine(TVult[TVult$rowNumber%in%c((rown[1]-1),(max(rown)+1)),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[1],c("location.long", "location.lat")]))
+      ### calculate total distance between fix before/after and the second alternate location
+      dist2 <- sum(distHaversine(TVult[TVult$rowNumber%in%c((rown[1]-1),(max(rown)+1)),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[2],c("location.long", "location.lat")]))
+      ### omit the alternate location that produces the longer route
+      if(dist1<dist2){rowsTOremove <- rown[2]}else{rowsTOremove <- rown[1]}
+    }
+    ## in case the duplicated time stamps are the very first location of the dataset, calculate distance between duplicate and following location
+    if(TVult$rowNumber[1]==rown[1]){
+      dist1 <- sum(distHaversine(TVult[TVult$rowNumber==(max(rown)+1),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[1],c("location.long", "location.lat")]))
+      dist2 <- sum(distHaversine(TVult[TVult$rowNumber==(max(rown)+1),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[2],c("location.long", "location.lat")]))
+      ### and omit the alternate location that produces the longer route
+      if(dist1<dist2){rowsTOremove <- rown[2]}else{rowsTOremove <- rown[1]}
+    }
+    ## in case the duplicated time stamps are the very last positions, calculate distance between duplicate and previous location
+    if(TVult$rowNumber[nrow(TVult)]==max(rown)){
+      dist1 <- sum(distHaversine(TVult[TVult$rowNumber==(rown[1]-1),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[1],c("location.long", "location.lat")]))
+      dist2 <- sum(distHaversine(TVult[TVult$rowNumber==(rown[1]-1),c("location.long", "location.lat")],
+                                 TVult[TVult$rowNumber==rown[2],c("location.long", "location.lat")]))
+      ### and omit the alternate location that produces the longer route
+      if(dist1<dist2){rowsTOremove <- rown[2]}else{rowsTOremove <- rown[1]}
+    }
+    return(rowsTOremove)
+  })
+  TVult <- TVult[-unique(sort(unlist(allrowsTOremove))),]
+  TVult$rowNumber <- NULL
+}
+
+## 3. Make a Move object and save it in the directory -------------------------------
+
+### Convert the dataset to the Move class
+TVult.move <- move(x = TVult$location.long, y = TVult$location.lat, time = TVult$timestamp,
+                   data = TVult, proj = crs("+proj=longlat +datum=WGS84"),
+                   animal = TVult$individual.local.identifier, sensor = TVult$sensor.type)
+
+### Save the resulting Move object
+#### IMPORTANT: CHANGE the file name of each Move Object to be consistent with the animal ID
+save(TVult.move, file = "Data_Processed/Cln.Vulture7xxx.Rdata")
+
+
+# `Move2` (New) Workflow ----------------------------------------------------------------------------
+## Packages
+
+library(move2)
+
+## 1. Import the data -------------------------------------------------------------------------------
+## The data has been downloaded from the Movebank repository in '.csv' format
+
+### Directly search and choose the '.csv' files in the directory
+TVult1 <- read.csv(choose.files())
+### Check the structure of the dataset
+str(TVult1)
